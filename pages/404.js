915cbdd5ff37fb43bde9413ed900cf5769db7642
SaulLudena <cornejo758@gmail.com>
2025-10-17 00:31:02 -0500

    adding responsive layout

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,12 @@
+import Link from "next/link";
 export default function Custom404() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center text-white ">
       <h1 className="text-6xl font-bold">What are you looking for</h1>
       <p className="mt-4 text-xl">Ufos exists in S4 Dulce base.</p>
-      <a href="/" className="mt-6 text-[#00DF82] hover:underline">
+      <Link href="/" className="mt-6 text-[#00DF82] hover:underline">
         Go back home
-      </a>
+      </Link>
     </div>
   );
 }
